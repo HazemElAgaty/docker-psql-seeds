9043552b8f9c26c4df3eae0db2febf8d00336c2f
Hazem El Agaty <hazemelagaty@gmail.com>
2018-05-03 00:33:00 +0200

    add seeds to user-service

---
+++ services/user-service/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO app_user
+(id, user_name, email, password) VALUES (1, 'mahmooud','7oda@hotmail.com','gibberish');
+
+INSERT INTO app_user
+(id, user_name, email, password) VALUES (2, 'hazem','zoma@hotmail.com','gibberish');
