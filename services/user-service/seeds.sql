76b5f7c48c5b609043130b90a83ee0aa0b6652dd
Hazem El Agaty <hazemelagaty@gmail.com>
2018-05-03 00:40:05 +0200

    add salt

--- services/user-service/seeds.sql
+++ services/user-service/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO app_user
-(id, user_name, email, password) VALUES (1, 'mahmooud','7oda@hotmail.com','gibberish');
+(id, user_name, email, password, salt) VALUES (1, 'mahmooud','7oda@hotmail.com','gibberish','0');
 
 INSERT INTO app_user
-(id, user_name, email, password) VALUES (2, 'hazem','zoma@hotmail.com','gibberish');
+(id, user_name, email, password, salt) VALUES (2, 'hazem','zoma@hotmail.com','gibberish', '0');
